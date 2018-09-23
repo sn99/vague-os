31b4560fd62bec80ff0756bf949f7e87a61f0c39
sn99 <siddharthn.099@gmail.com>
2018-09-23 22:14:19 +0530

    made a free standing rust library

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # vague-os
 
-A hobby OS written in Rust
+A hobby OS written in Rust inspired from(or copied for now) https://os.phil-opp.com/
+**Basically a dump to everything I learn about OS dev**
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+// function called on Panic
+#[panic_handler]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
+}
